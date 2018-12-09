a4e61541d6314d097277e6697741860120579b21
owlsogul <owlsogul@naver.com>
2018-12-09 17:25:21 +0900

    로그인/회원가입 액티비티, 모델 구현

--- app/src/main/java/github/a3sung/dreammemo/SignInActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/SignInActivity.java
@@ -1,13 +1,91 @@
 package github.a3sung.dreammemo;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import github.a3sung.dreammemo.account.AccountController;
+import github.a3sung.dreammemo.serverconnector.RequestCallback;
+import github.a3sung.dreammemo.serverconnector.ServerConnector;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText txtId;
+    private EditText txtPw;
+
+    private Handler loginSuccessHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_LONG).show();
+            finish();
+            Intent goToDreamPostListActivity = new Intent(SignInActivity.this, DreamPostListActivity.class);
+            startActivity(goToDreamPostListActivity);
+        }
+    };
+
+    private Handler loginFailHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        txtId = findViewById(R.id.txtId);
+        txtPw = findViewById(R.id.txtPw);
+    }
+
+    public void onClickSignIn(View view){
+        ServerConnector svConn = ServerConnector.getInstatnce();
+        String userId = txtId.getText().toString();
+        String userPw = txtPw.getText().toString();
+
+        svConn.requestGet(ServerConnector.BASE_URL + String.format("signin?userID=%s&userPW%s", userId, userPw), new RequestCallback() {
+            @Override
+            public void requestCallback(String result) {
+                try {
+                    JSONObject resultObj = new JSONObject(result);
+                    String token = resultObj.getString("token");
+                    Log.d("Account", "token info : " + token);
+                    if (validateToken(token)){
+                        AccountController.getInstance().signIn(token);
+                        Message msg = loginSuccessHandler.obtainMessage();
+                        loginSuccessHandler.sendMessage(msg);
+                    }
+                    else {
+                        // TODO: (중요도1) 로그인 실패 시 로그인 제한 되게 하기
+                        Message msg = loginFailHandler.obtainMessage();
+                        loginFailHandler.sendMessage(msg);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private boolean validateToken(String token){
+        // TODO: (중요도5) 로그인 성공한 토큰인지 아닌지 확인
+        return true;
+    }
+
+    public void onClickSignUp(View view){
+        Intent toSignUp = new Intent(SignInActivity.this, SignUpActivity.class);
+        toSignUp.putExtra("userId", txtId.getText().toString());
+        startActivity(toSignUp);
     }
 }
--- app/src/main/java/github/a3sung/dreammemo/SignUpActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/SignUpActivity.java
@@ -1,13 +1,82 @@
 package github.a3sung.dreammemo;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import github.a3sung.dreammemo.serverconnector.RequestCallback;
+import github.a3sung.dreammemo.serverconnector.ServerConnector;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText txtId;
+    private EditText txtPw;
+    private EditText txtPwConfirmed;
+    private EditText txtEmail;
+
+    private Handler signUpSuccessHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(getApplicationContext(), "회원가입 성공", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        txtId = findViewById(R.id.txtId);
+        txtPw = findViewById(R.id.txtPw);
+        txtPwConfirmed = findViewById(R.id.txtPwConfirmed);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        Intent intent = getIntent();
+        String userId = intent.getExtras().getString("userId");
+        if (userId != null && !userId.isEmpty()){
+            txtId.setText(userId);
+        }
     }
+
+    public void onClickSignUp(View view){
+
+        String userId = txtId.getText().toString();
+        String userPw = txtPw.getText().toString();
+        String userPwConfirmed = txtPwConfirmed.getText().toString();
+        String userEmail = txtEmail.getText().toString();
+
+        if (!validateForm(userId, userPw, userPwConfirmed, userEmail)){
+            return;
+        }
+
+        ServerConnector svConn = ServerConnector.getInstatnce();
+        svConn.requestPost(ServerConnector.BASE_URL + "signIn", String.format("userID=%s&userEmail=%s&userPW=%s", userId, userEmail, userPw), new RequestCallback() {
+            @Override
+            public void requestCallback(String result) {
+                Log.d("Account", "Sign Up success!");
+                Message msg = signUpSuccessHandler.obtainMessage();
+                signUpSuccessHandler.sendMessage(msg);
+            }
+        });
+
+
+    }
+
+    private boolean validateForm(String userId, String userPw, String userPwConfirmed, String userEmail){
+        if (!userPw.equals(userPwConfirmed)){
+            Toast.makeText(getApplicationContext(), "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
 }
