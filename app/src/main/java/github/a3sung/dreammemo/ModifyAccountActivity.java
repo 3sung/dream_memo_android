a75e2a3bd658ce5644d10bd15646976214d3e01c
owlsogul <owlsogul@naver.com>
2018-12-09 22:27:20 +0900

    회원정보 수정 페이지

--- app/src/main/java/github/a3sung/dreammemo/ModifyAccountActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/ModifyAccountActivity.java
@@ -2,12 +2,37 @@ package github.a3sung.dreammemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ModifyAccountActivity extends AppCompatActivity {
+
+    private EditText txtPw;
+    private EditText txtPwConfirmed;
+    private Button btnChangePw;
+    private Button btnQuit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_account);
+
+        txtPw = findViewById(R.id.txtPw);
+        txtPwConfirmed = findViewById(R.id.txtPwConfirmed);
+        btnChangePw = findViewById(R.id.btnChangePw);
+        btnQuit = findViewById(R.id.btnQuit);
+
+    }
+
+    public void onClickChangePw(View view){
+
+
+
+    }
+
+    public void onClickQuit(View view){
+
     }
 }
