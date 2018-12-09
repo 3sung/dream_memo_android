de21af1e0112e6221e9b7d70b28fe4d761f16229
owlsogul <owlsogul@naver.com>
2018-12-09 13:50:08 +0900

    웹서버 통신 모듈 완성

--- app/src/main/java/github/a3sung/dreammemo/MemoListActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/MemoListActivity.java
@@ -2,6 +2,10 @@ package github.a3sung.dreammemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import github.a3sung.dreammemo.serverconnector.RequestCallback;
+import github.a3sung.dreammemo.serverconnector.ServerConnector;
 
 public class MemoListActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MemoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_list);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TTESTT","resume");
+        ServerConnector svConn = ServerConnector.getInstatnce();
+        svConn.requestPost(ServerConnector.BASE_URL + "signUp", String.format("userID=%s&userEmail=%s&userPW=%s", "test", "test@test.com", "test"), new RequestCallback() {
+            @Override
+            public void requestCallback(String result) {
+                Log.d("TTESTT","register result " + result);
+            }
+        });
+        svConn.requestGet(ServerConnector.BASE_URL + "signIn?userID=test&userPW=test", new RequestCallback() {
+            @Override
+            public void requestCallback(String result) {
+                Log.d("TTESTT","login result " + result);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TTESTT","pause");
     }
 }
