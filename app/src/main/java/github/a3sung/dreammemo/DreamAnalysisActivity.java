8c0520a381abb49644a217ef77957c28ce3175f1
owlsogul <owlsogul@naver.com>
2018-12-09 19:26:25 +0900

    해몽 액티비티 완성

--- app/src/main/java/github/a3sung/dreammemo/DreamAnalysisActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/DreamAnalysisActivity.java
@@ -1,13 +1,83 @@
 package github.a3sung.dreammemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+
+import github.a3sung.dreammemo.serverconnector.RequestCallback;
+import github.a3sung.dreammemo.serverconnector.ServerConnector;
 
 public class DreamAnalysisActivity extends AppCompatActivity {
+
+    private TextView labelContent;
+    private TextView labelTag;
+
+    private LinkedList<String> tags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dream_analysis);
+
+        tags = new LinkedList<>();
+        labelContent = findViewById(R.id.labelContent);
+        labelTag = findViewById(R.id.labelTag);
+        labelContent.setText("");
+        labelTag.setText("");
+
+        // init tag text
+        Intent fromIntent = getIntent();
+        String tagStr = fromIntent.getExtras().getString("tag");
+        try {
+            if (tagStr != null){
+                JSONArray tagObj = new JSONArray(tagStr);
+                for (int i = 0; i < tagObj.length(); i++){
+                    tags.add(tagObj.getString(i));
+                    labelTag.append(tagObj.getString(i));
+                    if (i != tagObj.length()-1){
+                        labelTag.append(", ");
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        labelContent.setText("해몽 데이터를 가져오는 중입니다.");
+
+        JSONArray jArr = new JSONArray();
+        for (String tag : tags){
+            jArr.put(tag);
+        }
+
+        ServerConnector svConn = ServerConnector.getInstatnce();
+        svConn.requestPost(ServerConnector.BASE_URL + "dream_analysis", "tag=" + jArr.toString(), new RequestCallback() {
+            @Override
+            public void requestCallback(String result) {
+                try {
+                    JSONObject jObj = new JSONObject(result);
+                    for (String tag : tags){
+                        String content = jObj.getString(tag);
+                        labelContent.append(tag+"의 풀이\n");
+                        if (content != null){
+                            labelContent.append(content);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
     }
 }
