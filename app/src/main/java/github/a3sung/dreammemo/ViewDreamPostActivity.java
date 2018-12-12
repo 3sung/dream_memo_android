10e33d58b93c102ada1f4dc3d840e9fa1988c57d
Mingyu Jo <owlsogul@naver.com>
2018-12-13 04:21:04 +0900

    drawer 에러부분

--- app/src/main/java/github/a3sung/dreammemo/ViewDreamPostActivity.java
+++ app/src/main/java/github/a3sung/dreammemo/ViewDreamPostActivity.java
@@ -78,12 +78,7 @@ public class ViewDreamPostActivity extends AppCompatActivity implements Navigati
     }
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+
     }
 
     @Override
